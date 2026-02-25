98ad6ef1ddeca60ddeb1c5b59f5d9e32d2b64661
GuilhermePelissari <guilhermepelissari.feitosa@autbank.com.br>
2026-02-25 08:59:11 -0300

    first commit

---
+++ tabelaFipe/src/main/java/br/com/alura/tabelaFipe/principal/Principal.java
@@ -0,0 +1,41 @@
+package br.com.alura.tabelaFipe.principal;
+
+import br.com.alura.tabelaFipe.service.ConsumoApi;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+    public void exibeMenu(){
+        var menu = """
+                *** OPÇÕES ***
+                Carro
+                Moto
+                Caminhão
+                
+                Digite uma das opções para consultar a tabela Fipe:
+                """;
+
+        System.out.println(menu);
+        var opcao = leitura.nextLine();
+        String endereco;
+
+        if(opcao.toLowerCase().contains("carr")){
+            endereco = URL_BASE + "carros/marcas";
+        } else if(opcao.toLowerCase().contains("mot")){
+            endereco = URL_BASE + "motos/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+        var json = consumo.obterDados(endereco);
+        System.out.println(json);
+
+
+
+    }
+}
